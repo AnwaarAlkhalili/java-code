3bc4a8e441c470e9f91fd41689ac5bed9e936097
hp <hp@Anwaar-Al-Khalili>
2025-04-11 19:48:34 +0400

    spring boot

--- src/main/java/com/training/springboot/Application.java
+++ src/main/java/com/training/springboot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoolApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoolApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
